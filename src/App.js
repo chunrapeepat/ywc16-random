e5f291f2f71b1d596d894030e8d93539f371f202
chunza2542 <chunza2542@hotmail.com>
2018-04-29 02:38:42 +0700

    :lipstick: create Heading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import styled, { injectGlobal } from 'styled-components'
+
+import Heading from './components/Heading'
+
+injectGlobal`
+  body {
+    margin: 0;
+    background: black;
+    font-family: sans-serif;
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <div>
-        what the
+        <Heading text="Junior Webmaster Camp #10"/>
       </div>
     )
   }
---
+++ src/components/Heading.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.h1`
+  color: white;
+  position: block;
+  font-size: 50px;
+  margin: 100px 0;
+  text-align: center;
+  font-family: 'Roboto Slab', serif;
+`
+
+export default ({ text }) => (
+  <Heading>
+    {text}
+  </Heading>
+)
