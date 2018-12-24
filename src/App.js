3e81017de5f503f9f083583a7f544dc029ebd64d
chunza2542 <chunza2542@hotmail.com>
2018-12-25 01:12:03 +0700

    removed unused code

--- src/App.js
+++ src/App.js
@@ -11,28 +11,13 @@ injectGlobal`
   }
 `
 
-
-const Background = styled.div`
-  z-index: -999;
-  position: absolute;
-
-  width: 100vw;
-  height: 100vh;
-
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  background: linear-gradient(10deg, #00152a, #3E0009);
-`
-
 const Rotate = styled.div`
   height: 100vh;
   transform: rotate(-30deg);
 `
 
 const Container = styled.div`
+  height: 100vh;
   overflow: hidden;  
   z-index: 100;
 `
@@ -41,9 +26,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <Background /> */}
-        <video id="background-video"style={{ zIndex: -100, position:'absolute'}} loop src="assets/video/bg-tower-random.mp4"></video>
         <Container>
+          <video id="background-video" style={{ zIndex: -100, position:'absolute'}} loop src="assets/video/bg-tower-random.mp4"></video>
           <Rotate>
             <Random />
           </Rotate>
